0dd4aa446775a606f3ce883626c065f510b1434e
doranakan <orlandi.m88@gmail.com>
2024-12-09 18:53:09 +0100

    feat: user registration trigger

---
+++ supabase/migrations/20241209175120_user_registration_trigger.sql
@@ -0,0 +1,16 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_user_registration()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+    -- You can add your logic here, for example, inserting a record into a profiles table
+    insert into public.users (id, name, created_at)
+    values (new.id, new.raw_user_meta_data->>'name', now());
+    
+    return new;
+end;$function$
+;
+
+
